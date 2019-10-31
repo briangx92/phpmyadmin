8b60315f2b0041e9cddc33d0705cb4d4372409cc
Brian Gomez <briangx92@gmail.com>
2019-10-31 19:52:37 -0400

    Massive overhaul

--- dbIndex.php
+++ dbIndex.php
@@ -6,64 +6,17 @@ include_once 'db.php';
 $create = isset($_POST['create']);
 $insert = isset($_POST['insert']);
 $search = isset($_POST['search']);
+if ($create) {
 
-// FUNCTION FOR CREATING A TABLE
-function createTable($create) {
-
-    if ($create) {
-        $tb_name = $_POST['tb_name'];
-        $tb_col1 = $_POST['tb_col1'];
-        $tb_col2 = $_POST['tb_col2'];
-        $tb_col3 = $_POST['tb_col3'];
-        $tb_col4 = $_POST['tb_col4'];
-         
-        $sql_tb_table = "CREATE TABLE `$tb_name` ('$tb_col1' varchar(25), '$tb_col2' varchar(25), '$tb_col3' varchar(25), '$tb_col4' varchar(25);"; 
-
-    }
-
+    $sql_table = "CREATE TABLE `{$_POST['tb_name']}` (`{$_POST['tb_col1']}` varchar(25), `{$_POST['tb_col2']}` varchar(25),`{$_POST['tb_col3']}` varchar(25), `{$_POST['tb_col4']}` varchar(25));"; 
+    mysqli_query($conn, $sql_table);
 }
 
-// FUNCTION FOR INSERTING VALUES IN A TABLE
-function insertTable($insert,$create) {
-
-    if ($insert || $create) {
-        // Columns
-        $tb_col1 = $_POST['tb_col1'];
-        $tb_col2 = $_POST['tb_col2'];
-        $tb_col3 = $_POST['tb_col3'];
-        $tb_col4 = $_POST['tb_col4'];
-
-        // Insert values
-        $ins_name = $_POST['ins_name'];
-        $ins_val1 = $_POST['ins_val1'];
-        $ins_val2 = $_POST['ins_val2'];
-        $ins_val3 = $_POST['ins_val3'];
-        $ins_val4 = $_POST['ins_val4'];
-        
-        // Query
-        $sql_ins_table = "INSERT INTO `$ins_name` (`$tb_col1`, `$tb_col2`, `$tb_col3`, `$tb_col4`) VALUES ('$ins_val1', '$ins_val2', '$ins_val3', '$ins_val4');";
-
-
-    }
-
+if ($_POST) {
+$sql_insert = "INSERT INTO `{$_POST['tb_name']}` (`{$_POST['tb_col1']}`, `{$_POST['tb_col2']}`, `{$_POST['tb_col3']}`, `{$_POST['tb_col4']}`) VALUES (`{$_POST['ins_val1']}`, `{$_POST['ins_val2']}`, `{$_POST['ins_val3']}`, `{$_POST['ins_val4']}`);";
+    
 }
 
-
-// FUNCTION FOR SEARCHING
-function searchTable($search) {
-
-    if ($search) {
-        $sch_name = $_POST['sch_name'];
-        $sch_col1 = $_POST['sch_col1'];
-        $sch_col2 = $_POST['sch_col2'];
-        $sch_col3 = $_POST['sch_col3'];
-        $sch_col4 = $_POST['sch_col4'];
-
-
-
-
-    }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -91,8 +44,6 @@ function searchTable($search) {
     <form action="dbIndex.php" method="post">
         <legend>Insert Data</legend>
         <fieldset>
-            <input type="text" name="ins_name" placeholder="Table Name">
-            <br>
             <input type="text" name="ins_val1" placeholder="Column 1">
             <input type="text" name="ins_val2" placeholder="Column 2">
             <input type="text" name="ins_val3" placeholder="Column 3">
@@ -100,7 +51,7 @@ function searchTable($search) {
             <button type="submit" name="insert" value="insert">Insert</button>
         </fieldset>
     </form>
-    <!-- Search Data -->
+    Search Data 
     <form action="dbIndex.php" method="post">
         <legend>Search</legend>
         <fieldset>
@@ -118,9 +69,5 @@ function searchTable($search) {
 
 
 <?php
-
-// $sql = "INSERT INTO `customer` (`ID`, `fName`, `lName`, `Street`, `City`, `State`) VALUES ('10003', 'Leroy', 'Jenkins', 'Colonial Village', 'Hershey', 'VA');";
-// mysqli_query($conn, $sql);
-
 
 ?>
