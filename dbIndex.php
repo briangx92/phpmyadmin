4b7e6a817bc8314830c80b03751a681860925c45
Brian Gomez <briangx92@gmail.com>
2019-11-01 09:29:48 -0400

    Finished

--- dbIndex.php
+++ dbIndex.php
@@ -7,13 +7,50 @@ $create = isset($_POST['create']);
 $insert = isset($_POST['insert']);
 $search = isset($_POST['search']);
 if ($create) {
+    $tb_name = $_POST['tb_name'];
+    $tb_col1 = $_POST['tb_col1'];
+    $tb_col2 = $_POST['tb_col2'];
+    $tb_col3 = $_POST['tb_col3'];
+    $tb_col4 = $_POST['tb_col4'];
 
-    $sql_table = "CREATE TABLE `{$_POST['tb_name']}` (`{$_POST['tb_col1']}` varchar(25), `{$_POST['tb_col2']}` varchar(25),`{$_POST['tb_col3']}` varchar(25), `{$_POST['tb_col4']}` varchar(25));"; 
+    $sql_table = "CREATE TABLE `{$tb_name}` (`{$tb_col1}` varchar(25), `{$tb_col2}` varchar(25),`{$tb_col3}` varchar(25), `{$tb_col4}` varchar(25));"; 
     mysqli_query($conn, $sql_table);
+    print_r($_POST);
+}
+if ($insert) {
+    $ins_tbname = $_POST['ins_tbname'];
+    $ins_val1 = $_POST['ins_val1'];
+    $ins_val2 = $_POST['ins_val2'];
+    $ins_val3 = $_POST['ins_val3'];
+    $ins_val4 = $_POST['ins_val4'];
+
+    $sql_insert = "INSERT INTO `{$ins_tbname}` VALUES ('{$ins_val1}', '{$ins_val2}', '{$ins_val3}', '{$ins_val4}');";
+
+    mysqli_query($conn, $sql_insert);
+
 }
 
+if ($search) {
 
+    $sch_name = $_POST['sch_name'];
+    $sch_col1 = $_POST['sch_col1'];
+    $sch_col2 = $_POST['sch_col2'];
+    $sch_col3 = $_POST['sch_col3'];
+    $sch_col4 = $_POST['sch_col4'];
 
+    $sql_search = "SELECT * FROM {$sch_name};";
+
+    $result = mysqli_query($conn, $sql_search);
+
+    while($row = mysqli_fetch_assoc($result)){
+        foreach($row as $fields => $value) {
+            echo $value . "\n";
+        }
+
+    }
+
+    
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,10 +79,11 @@ if ($create) {
     <form action="dbIndex.php" method="post">
         <legend>Insert Data</legend>
         <fieldset>
-            <input type="text" name="ins_val1" placeholder="Column 1">
-            <input type="text" name="ins_val2" placeholder="Column 2">
-            <input type="text" name="ins_val3" placeholder="Column 3">
-            <input type="text" name="ins_val4" placeholder="Column 4">
+            <input type="text" name="ins_tbname" placeholder="Table Name">
+            <input type="text" name="ins_val1" placeholder="Value 1">
+            <input type="text" name="ins_val2" placeholder="Value 2">
+            <input type="text" name="ins_val3" placeholder="Value 3">
+            <input type="text" name="ins_val4" placeholder="Value 4">
             <br>
             <button type="submit" name="insert" value="insert">Insert</button>
         </fieldset>
@@ -53,7 +91,7 @@ if ($create) {
     <form action="dbIndex.php" method="post">
         <legend>Search</legend>
         <fieldset>
-            <input type="text" name="sch_name" placeholder="Search Data">
+            <input type="text" name="sch_name" placeholder="Table Name">
             <br>
             <input type="text" name="sch_col1" placeholder="Column 1">
             <input type="text" name="sch_col2" placeholder="Column 2">
